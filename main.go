9e07c37d03504483c3a7633c0b8fe2784f63baf6
taira120 <taira1999120@gmail.com>
2019-11-05 18:28:28 +0900

    最低限の機能を実装
    - 入力を読み込む
    - 入力をコマンド名と引数に分ける
    - 実行する
    - 実行結果をstdoutとstderrに分けて出力する

--- main.go
+++ main.go
@@ -2,21 +2,75 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"os"
+	"os/exec"
+	"strings"
 )
 
 func main() {
-	gsh_loop()
+	stdin := bufio.NewReader(os.Stdin)
+	stdout := os.Stdout
+	stderr := os.Stderr
+	gsh_loop(stdin, stdout, stderr)
 }
 
-func gsh_loop() {
-	var err error
-	reader := bufio.NewReader(os.Stdin)
+func gsh_loop(stdin *bufio.Reader, stdout io.Writer, stderr io.Writer) {
+	for {
+		fmt.Fprint(stdout, "> ")
+		line, err := gsh_read_line(stdin)
+		if err != nil {
+			fmt.Fprint(stderr, "Couldn't read your input")
+		}
 
-	for err == nil {
-		fmt.Print("> ")
-		text, _ := reader.ReadString('\n')
-		fmt.Println(text)
+		name, args := gsh_split_line(line)
+		gsh_execute(stdout, stderr, name, args)
+	}
+}
+
+func gsh_read_line(stdin *bufio.Reader) (line string, err error) {
+	return stdin.ReadString('\n')
+}
+
+func gsh_split_line(line string) (name string, args []string) {
+	trimmedLine := bytes.TrimSpace([]byte(line))
+	result := strings.Split(string(trimmedLine), " ")
+	if len(result) > 1 {
+		return result[0], result[1:len(result)]
+	} else {
+		return result[0], nil
+	}
+}
+
+func gsh_execute(stdout io.Writer, stderr io.Writer, name string, args []string) {
+	cmd := exec.Command(name, args...)
+
+	stdoutPipe, err := cmd.StdoutPipe()
+	if err != nil {
+		fmt.Fprintln(stderr, err)
+	}
+
+	stderrPipe, err := cmd.StderrPipe()
+	if err != nil {
+		fmt.Fprintln(stderr, err)
+	}
+
+	if err := cmd.Start(); err != nil {
+		fmt.Fprintln(stderr, err)
+		return
+	}
+
+	out, _ := ioutil.ReadAll(stdoutPipe)
+	fmt.Fprint(stdout, string(out))
+	slurp, _ := ioutil.ReadAll(stderrPipe)
+	if len(slurp) > 0 {
+		fmt.Fprintln(stderr, string(slurp))
+	}
+
+	if err := cmd.Wait(); err != nil {
+		fmt.Fprintln(stderr, err)
 	}
 }
